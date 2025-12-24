44166e0588d1270e923fa8dca1e25f2f083949e6
elgnas <sangle1001.dev@gmail.com>
2025-12-24 17:48:29 +0700

    feat: migrate to webpack for build and development
    - Updated package.json to replace lite-server with webpack-dev-server for development.
    - Added build script using webpack for production builds.
    - Introduced webpack configuration files for both development and production environments.
    - Refactored import statements in TypeScript files to remove file extensions.

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,7 +1,7 @@
-import { projectState } from "../state/project-state.js";
-import { Validatable, validate } from "../util/validation.js";
-import { Component } from "./base-component.js";
-import { autobind } from "../decorators/autobind.js";
+import { projectState } from "../state/project-state";
+import { Validatable, validate } from "../util/validation";
+import { Component } from "./base-component";
+import { autobind } from "../decorators/autobind";
 
 export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
   titleInputElement: HTMLInputElement;
